7143a7bff62968e5889585e89ca20d8ae2f5109a
Nikita <tnt.nikitik@gmail.com>
2023-12-13 18:20:36 +0300

    unittest ch

--- unittest.cpp
+++ unittest.cpp
@@ -8,12 +8,14 @@ using namespace std;
 SUITE(KeyTest)
 {
     TEST(ValidKey) {
-        tableCipher test(5);
-        CHECK(true);
+        wstring open_text = L"ПРИВЕТ";
+        wstring result_correct = L"Е_В_И_Р_ПТ";
+        CHECK_EQUAL(true, result_correct == tableCipher(5).encrypt(open_text));
     }
     TEST(LongKey) {
-        tableCipher test(100);
-        CHECK(true);
+        wstring open_text = L"ПРИВЕТ";
+        wstring result_correct = L"_ТЕВИРП";
+        CHECK_EQUAL(true, result_correct == tableCipher(7).encrypt(open_text));
     }
     /*TEST(MinusInKey) {
         CHECK_THROW(tableCipher test(-245),cipher_error);
@@ -41,8 +43,8 @@ SUITE(EncryptTest)
         CHECK_EQUAL(true, result_correct == pointer->encrypt(open_text));
     }
     /*TEST_FIXTURE(KeyAB_fixture, LowText) {
-        wstring open_text = L"съешьжеещёэтихмягкихфранцузскихбулокдавыпейчаю";
-        wstring result_correct = L"ШЕТЯХНСБКЫЧ_ЕЕЭМИАЗХОВЙ_ЪЖЁХКРУИЛАЕЮСЬЩИГФЦКУДПА";
+        wstring open_text = L"hello";
+        wstring result_correct = L"L_L_E_HO";
         CHECK_EQUAL(true, result_correct == pointer->encrypt(open_text));
     }
     TEST_FIXTURE(KeyAB_fixture, TextWithoutletters) {
@@ -52,9 +54,6 @@ SUITE(EncryptTest)
     TEST_FIXTURE(KeyAB_fixture, TextWithNumber) {
         wstring open_text = L"1337H4cK3r";
         CHECK_THROW(pointer->encrypt(open_text), cipher_error);
-    }
-    TEST_FIXTURE(KeyAB_fixture, TextWithPunct) {
-        j _error);
     }*/
 }
 SUITE(DecryptText)
@@ -65,11 +64,9 @@ SUITE(DecryptText)
         CHECK_EQUAL(true, result_correct == pointer->decrypt(open_text));
     }
     /*TEST_FIXTURE(KeyAB_fixture, LowTEXT) {
-        CHECK_EQUAL(wst1(4,L"PriaRARNomoIGMve"),"PROGRAMMIROVANIE");
-    }
-    TEST_FIXTURE(KeyAB_fixture, EmptyTEXT) {
-        wstring open_text = L"";
-        CHECK_THROW(pointer->decrypt(open_text), cipher_error);
+        wstring open_text = L"l_l_e_ho";
+        wstring result_correct = L"HELLO";
+        CHECK_EQUAL(true, result_correct == pointer->decrypt(open_text));
     }
     TEST_FIXTURE(KeyAB_fixture, TextNumberText) {
         wstring open_text = L"ВИРТПЕ132";
@@ -78,9 +75,9 @@ SUITE(DecryptText)
     TEST_FIXTURE(KeyAB_fixture, TextSymbolText) {
         wstring open_text = L"ВИРТПЕ!@#$";
         CHECK_THROW(pointer->decrypt(open_text), cipher_error);
-    }
+    }*/
 
-}*/
+}
 
 int main()
 {
